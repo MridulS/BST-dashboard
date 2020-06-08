e1ff049ed33e34becbc47e93faf3908e25009b6f
Mridul Seth <seth.mridul@gmail.com>
2020-06-08 17:51:42 +0530

    fix 3rd fig

--- BST-dashboard.py
+++ BST-dashboard.py
@@ -31,19 +31,27 @@
 #
 # [![econ-ark.org](https://img.shields.io/badge/Powered%20by-Econ--ARK-3e8acc.svg)](https://econ-ark.org/materials/BufferStockTheory)
 
-# %% [markdown]
-#
-
 # %%
 from ipywidgets import interact, interactive, fixed, interact_manual
-from dashboard_widget import (makeGICFailExample, makeConvergencePlot, DiscFac_widget,
-CRRA_widget, Rfree_widget, PermGroFac_widget,UnempPrb_widget,IncUnemp_widget, makesomethingplot)  
+from dashboard_widget import (
+    makeGICFailExample,
+    makeConvergencePlot,
+    DiscFac_widget,
+    CRRA_widget,
+    Rfree_widget,
+    PermGroFac_widget,
+    UnempPrb_widget,
+    IncUnemp_widget,
+    makesomethingplot,
+)
 
 # %%
 import HARK
+
 HARK.logging.disable()
 # The warnings package allows us to ignore some harmless but alarming warning messages
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # %% [markdown]
@@ -57,12 +65,14 @@ warnings.filterwarnings("ignore")
 #
 
 # %%
-a = interactive(makeConvergencePlot,
-         DiscFac = DiscFac_widget,
-         CRRA = CRRA_widget,
-         Rfree = Rfree_widget,
-         PermGroFac = PermGroFac_widget,
-         UnempPrb = UnempPrb_widget)
+a = interactive(
+    makeConvergencePlot,
+    DiscFac=DiscFac_widget,
+    CRRA=CRRA_widget,
+    Rfree=Rfree_widget,
+    PermGroFac=PermGroFac_widget,
+    UnempPrb=UnempPrb_widget,
+)
 a
 
 # %% [markdown]
@@ -79,8 +89,7 @@ a
 #
 
 # %%
-b = interactive(makeGICFailExample, Rfree = Rfree_widget,
-         PermGroFac = PermGroFac_widget)
+b = interactive(makeGICFailExample, Rfree=Rfree_widget, PermGroFac=PermGroFac_widget)
 b
 
 
@@ -91,8 +100,13 @@ b
 #
 
 # %%
-c = interactive(makesomethingplot, Rfree = Rfree_widget,
-         PermGroFac = PermGroFac_widget, DiscFac = DiscFac_widget, CRRA = CRRA_widget)
+c = interactive(
+    makesomethingplot,
+    Rfree=Rfree_widget,
+    PermGroFac=PermGroFac_widget,
+    DiscFac=DiscFac_widget,
+    CRRA=CRRA_widget,
+)
 
 # %%
 c
@@ -107,60 +121,149 @@ c
 # %%
 # Define k_lower, h_inf and perfect foresight consumption function, upper bound of consumption function and lower
 # bound of consumption function.
-k_lower = 1.0-(baseEx_inf.Rfree**(-1.0))*(baseEx_inf.Rfree*baseEx_inf.DiscFac)**(1.0/baseEx_inf.CRRA)
-h_inf = (1.0/(1.0-baseEx_inf.PermGroFac[0]/baseEx_inf.Rfree))
-conFunc_PF = lambda m: (h_inf -1)* k_lower + k_lower*m
-conFunc_upper = lambda m: (1 - baseEx_inf.UnempPrb ** (1.0/baseEx_inf.CRRA)*(baseEx_inf.Rfree*baseEx_inf.DiscFac)**(1.0/baseEx_inf.CRRA)/baseEx_inf.Rfree)*m
-conFunc_lower = lambda m: (1 -(baseEx_inf.Rfree*baseEx_inf.DiscFac)**(1.0/baseEx_inf.CRRA)/baseEx_inf.Rfree) * m
-intersect_m = ((h_inf-1)* k_lower)/((1 - baseEx_inf.UnempPrb
-            **(1.0/baseEx_inf.CRRA)*(baseEx_inf.Rfree*baseEx_inf.DiscFac)**(1.0/baseEx_inf.CRRA)/baseEx_inf.Rfree)-k_lower)
+k_lower = 1.0 - (baseEx_inf.Rfree ** (-1.0)) * (
+    baseEx_inf.Rfree * baseEx_inf.DiscFac
+) ** (1.0 / baseEx_inf.CRRA)
+h_inf = 1.0 / (1.0 - baseEx_inf.PermGroFac[0] / baseEx_inf.Rfree)
+conFunc_PF = lambda m: (h_inf - 1) * k_lower + k_lower * m
+conFunc_upper = (
+    lambda m: (
+        1
+        - baseEx_inf.UnempPrb ** (1.0 / baseEx_inf.CRRA)
+        * (baseEx_inf.Rfree * baseEx_inf.DiscFac) ** (1.0 / baseEx_inf.CRRA)
+        / baseEx_inf.Rfree
+    )
+    * m
+)
+conFunc_lower = (
+    lambda m: (
+        1
+        - (baseEx_inf.Rfree * baseEx_inf.DiscFac) ** (1.0 / baseEx_inf.CRRA)
+        / baseEx_inf.Rfree
+    )
+    * m
+)
+intersect_m = ((h_inf - 1) * k_lower) / (
+    (
+        1
+        - baseEx_inf.UnempPrb ** (1.0 / baseEx_inf.CRRA)
+        * (baseEx_inf.Rfree * baseEx_inf.DiscFac) ** (1.0 / baseEx_inf.CRRA)
+        / baseEx_inf.Rfree
+    )
+    - k_lower
+)
 
 
 # %%
 # Plot the consumption function and its bounds
 
-cMaxLabel=r'$\overline{c}(m) = (m-1+h)\underline{\kappa}$'
-cMinLabel=r'$\underline{c}(m)= (1-\pmb{\text{\TH}}_{R})\underline{\kappa}m$'
+cMaxLabel = r"$\overline{c}(m) = (m-1+h)\underline{\kappa}$"
+cMinLabel = r"$\underline{c}(m)= (1-\pmb{\text{\TH}}_{R})\underline{\kappa}m$"
 if not latexExists:
-    cMaxLabel=r'$\overline{c}(m) = (m-1+h)κ̲' # Use unicode kludge
-    cMinLabel=r'c̲$(m)= (1-\Phi_{R})m = κ̲ m$'
+    cMaxLabel = r"$\overline{c}(m) = (m-1+h)κ̲"  # Use unicode kludge
+    cMinLabel = r"c̲$(m)= (1-\Phi_{R})m = κ̲ m$"
 
-x1 = np.linspace(0,25,1000)
-x3 = np.linspace(0,intersect_m,300)
-x4 = np.linspace(intersect_m,25,700)
+x1 = np.linspace(0, 25, 1000)
+x3 = np.linspace(0, intersect_m, 300)
+x4 = np.linspace(intersect_m, 25, 700)
 cfunc_m = baseEx_inf.cFunc[0](x1)
 cfunc_PF_1 = conFunc_PF(x3)
 cfunc_PF_2 = conFunc_PF(x4)
 cfunc_upper_1 = conFunc_upper(x3)
 cfunc_upper_2 = conFunc_upper(x4)
 cfunc_lower = conFunc_lower(x1)
-plt.figure(figsize = (12,8))
-plt.plot(x1,cfunc_m, color="black")
-plt.plot(x1,cfunc_lower, color="black",linewidth=2.5)
-plt.plot(x3,cfunc_upper_1, color="black",linewidth=2.5)
-plt.plot(x4,cfunc_PF_2 , color="black",linewidth=2.5)
-plt.plot(x4,cfunc_upper_2 , color="black",linestyle="--")
-plt.plot(x3,cfunc_PF_1 , color="black",linestyle="--")
-plt.tick_params(labelbottom=False, labelleft=False,left='off',right='off',bottom='off',top='off')
-plt.xlim(0,25)
-plt.ylim(0,1.12*conFunc_PF(25))
-plt.text(0,1.12*conFunc_PF(25)+0.05,"$c$",fontsize = 22)
-plt.text(25+0.1,0,"$m$",fontsize = 22)
-plt.text(2.5,1,r'$c(m)$',fontsize = 22,fontweight='bold')
+plt.figure(figsize=(12, 8))
+plt.plot(x1, cfunc_m, color="black")
+plt.plot(x1, cfunc_lower, color="black", linewidth=2.5)
+plt.plot(x3, cfunc_upper_1, color="black", linewidth=2.5)
+plt.plot(x4, cfunc_PF_2, color="black", linewidth=2.5)
+plt.plot(x4, cfunc_upper_2, color="black", linestyle="--")
+plt.plot(x3, cfunc_PF_1, color="black", linestyle="--")
+plt.tick_params(
+    labelbottom=False, labelleft=False, left="off", right="off", bottom="off", top="off"
+)
+plt.xlim(0, 25)
+plt.ylim(0, 1.12 * conFunc_PF(25))
+plt.text(0, 1.12 * conFunc_PF(25) + 0.05, "$c$", fontsize=22)
+plt.text(25 + 0.1, 0, "$m$", fontsize=22)
+plt.text(2.5, 1, r"$c(m)$", fontsize=22, fontweight="bold")
 if latexExists:
-    plt.text(6,5,r'$\overline{\overline{c}}(m)= \overline{\MPC}m = (1-\wp^{1/\CRRA}\pmb{\text{\TH}}_{R})m$',fontsize = 22,fontweight='bold')
+    plt.text(
+        6,
+        5,
+        r"$\overline{\overline{c}}(m)= \overline{\MPC}m = (1-\wp^{1/\CRRA}\pmb{\text{\TH}}_{R})m$",
+        fontsize=22,
+        fontweight="bold",
+    )
 else:
-    plt.text(6,5,r'$\overline{\overline{c}}(m)= \overline{\MPC}m = (1-\wp^{1/\CRRA}\Phi_{R})m$',fontsize = 22,fontweight='bold')
-plt.text(2.2,3.8, cMaxLabel,fontsize = 22,fontweight='bold')
-plt.text(9,4.1,r'Upper Bound $ = $ Min $[\overline{\overline{c}}(m),\overline{c}(m)]$',fontsize = 22,fontweight='bold')
-plt.text(7,0.7,cMinLabel,fontsize = 22,fontweight='bold')
-plt.arrow(2.45,1.05,-0.5,0.02,head_width= 0.05,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(2.15,3.88,-0.5,0.1,head_width= 0.05,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(8.95,4.15,-0.8,0.05,head_width= 0.05,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(5.95,5.05,-0.4,0,head_width= 0.05,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(14,0.70,0.5,-0.1,head_width= 0.05,width=0.001,facecolor='black',length_includes_head='True')
+    plt.text(
+        6,
+        5,
+        r"$\overline{\overline{c}}(m)= \overline{\MPC}m = (1-\wp^{1/\CRRA}\Phi_{R})m$",
+        fontsize=22,
+        fontweight="bold",
+    )
+plt.text(2.2, 3.8, cMaxLabel, fontsize=22, fontweight="bold")
+plt.text(
+    9,
+    4.1,
+    r"Upper Bound $ = $ Min $[\overline{\overline{c}}(m),\overline{c}(m)]$",
+    fontsize=22,
+    fontweight="bold",
+)
+plt.text(7, 0.7, cMinLabel, fontsize=22, fontweight="bold")
+plt.arrow(
+    2.45,
+    1.05,
+    -0.5,
+    0.02,
+    head_width=0.05,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    2.15,
+    3.88,
+    -0.5,
+    0.1,
+    head_width=0.05,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    8.95,
+    4.15,
+    -0.8,
+    0.05,
+    head_width=0.05,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    5.95,
+    5.05,
+    -0.4,
+    0,
+    head_width=0.05,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    14,
+    0.70,
+    0.5,
+    -0.1,
+    head_width=0.05,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
 
-make('cFuncBounds')
+make("cFuncBounds")
 
 
 # %% [markdown]
@@ -171,29 +274,69 @@ make('cFuncBounds')
 # %%
 # This just plots objects that have already been constructed
 
-m1 = np.linspace(0,4,1000)
+m1 = np.linspace(0, 4, 1000)
 cfunc_m = baseEx_inf.cFunc[0](m1)
-mSSfunc = lambda m:(baseEx_inf.PermGroFac[0]/baseEx_inf.Rfree)+(1.0-baseEx_inf.PermGroFac[0]/baseEx_inf.Rfree)*m
+mSSfunc = (
+    lambda m: (baseEx_inf.PermGroFac[0] / baseEx_inf.Rfree)
+    + (1.0 - baseEx_inf.PermGroFac[0] / baseEx_inf.Rfree) * m
+)
 mss = mSSfunc(m1)
-plt.figure(figsize = (12,8))
-plt.plot(m1,cfunc_m, color="black")
-plt.plot(m1,mss, color="black")
-plt.xlim(0,3)
-plt.ylim(0,1.45)
-plt.plot([baseEx_inf.solution[0].mNrmSS, baseEx_inf.solution[0].mNrmSS],[0,2.5],color="black",linestyle="--")
-plt.tick_params(labelbottom=False, labelleft=False,left='off',right='off',bottom='off',top='off')
-plt.text(0,1.47,r"$c$",fontsize = 26)
-plt.text(3.02,0,r"$m$",fontsize = 26)
+plt.figure(figsize=(12, 8))
+plt.plot(m1, cfunc_m, color="black")
+plt.plot(m1, mss, color="black")
+plt.xlim(0, 3)
+plt.ylim(0, 1.45)
+plt.plot(
+    [baseEx_inf.solution[0].mNrmSS, baseEx_inf.solution[0].mNrmSS],
+    [0, 2.5],
+    color="black",
+    linestyle="--",
+)
+plt.tick_params(
+    labelbottom=False, labelleft=False, left="off", right="off", bottom="off", top="off"
+)
+plt.text(0, 1.47, r"$c$", fontsize=26)
+plt.text(3.02, 0, r"$m$", fontsize=26)
 if latexExists:
-    plt.text(2.3,0.94,r'$\mathbb{E}_{t}[\Delta m_{t+1}] = 0$',fontsize = 22,fontweight='bold')
+    plt.text(
+        2.3,
+        0.94,
+        r"$\mathbb{E}_{t}[\Delta m_{t+1}] = 0$",
+        fontsize=22,
+        fontweight="bold",
+    )
 else:
-    plt.text(2.3,0.94,r'$\mathsf{E}_{t}[\Delta m_{t+1}] = 0$',fontsize = 22,fontweight='bold')
-plt.text(2.3,1.1,r"$c(m_{t})$",fontsize = 22,fontweight='bold')
-plt.text(baseEx_inf.solution[0].mNrmSS-0.05,-0.1, r"$\Target{m}$",fontsize = 26)
-plt.arrow(2.28,1.12,-0.1,0.03,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(2.28,0.97,-0.1,0.02,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
+    plt.text(
+        2.3,
+        0.94,
+        r"$\mathsf{E}_{t}[\Delta m_{t+1}] = 0$",
+        fontsize=22,
+        fontweight="bold",
+    )
+plt.text(2.3, 1.1, r"$c(m_{t})$", fontsize=22, fontweight="bold")
+plt.text(baseEx_inf.solution[0].mNrmSS - 0.05, -0.1, r"$\Target{m}$", fontsize=26)
+plt.arrow(
+    2.28,
+    1.12,
+    -0.1,
+    0.03,
+    head_width=0.02,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    2.28,
+    0.97,
+    -0.1,
+    0.02,
+    head_width=0.02,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
 
-make('cRatTargetFig')
+make("cRatTargetFig")
 
 
 # %% [markdown]
@@ -207,41 +350,85 @@ make('cRatTargetFig')
 
 # %%
 # The last figure shows the upper and lower limits of the MPC
-plt.figure(figsize = (12,8))
+plt.figure(figsize=(12, 8))
 # Set the plot range of m
-m = np.linspace(0.001,8,1000)
+m = np.linspace(0.001, 8, 1000)
 
 # Use the HARK method derivative to get the derivative of cFunc, and the values are just the MPC
 MPC = baseEx_inf.cFunc[0].derivative(m)
 
 # Define the upper bound of MPC
-MPCUpper = (1 - baseEx_inf.UnempPrb ** (1.0/baseEx_inf.CRRA)*(baseEx_inf.Rfree*baseEx_inf.DiscFac)**(1.0/baseEx_inf.CRRA)/baseEx_inf.Rfree)
+MPCUpper = (
+    1
+    - baseEx_inf.UnempPrb ** (1.0 / baseEx_inf.CRRA)
+    * (baseEx_inf.Rfree * baseEx_inf.DiscFac) ** (1.0 / baseEx_inf.CRRA)
+    / baseEx_inf.Rfree
+)
 
 # Define the lower bound of MPC
 MPCLower = k_lower
 
-kappaDef=r'$\underline{\kappa}\equiv(1-\pmb{\text{\TH}}_{R})$'
+kappaDef = r"$\underline{\kappa}\equiv(1-\pmb{\text{\TH}}_{R})$"
 if not latexExists:
-    kappaDef=r'κ̲$\equiv(1-\Phi_{R})$'
+    kappaDef = r"κ̲$\equiv(1-\Phi_{R})$"
 
-plt.plot(m,MPC,color = 'black')
-plt.plot([0,8],[MPCUpper,MPCUpper],color = 'black')
-plt.plot([0,8],[MPCLower,MPCLower],color = 'black')
-plt.xlim(0,8)
-plt.ylim(0,1)
-plt.text(1.5,0.6,r'$\MPC(m) \equiv c^{\prime}(m)$',fontsize = 26,fontweight='bold')
+plt.plot(m, MPC, color="black")
+plt.plot([0, 8], [MPCUpper, MPCUpper], color="black")
+plt.plot([0, 8], [MPCLower, MPCLower], color="black")
+plt.xlim(0, 8)
+plt.ylim(0, 1)
+plt.text(1.5, 0.6, r"$\MPC(m) \equiv c^{\prime}(m)$", fontsize=26, fontweight="bold")
 if latexExists:
-    plt.text(5,0.87,r'$(1-\wp^{1/\CRRA}\pmb{\text{\TH}}_{R})\equiv \overline{\MPC}$',fontsize = 26,fontweight='bold') # Use Thorn character
+    plt.text(
+        5,
+        0.87,
+        r"$(1-\wp^{1/\CRRA}\pmb{\text{\TH}}_{R})\equiv \overline{\MPC}$",
+        fontsize=26,
+        fontweight="bold",
+    )  # Use Thorn character
 else:
-    plt.text(5,0.87,r'$(1-\wp^{1/\CRRA}\Phi_{R})\equiv \overline{\MPC}$',fontsize = 26,fontweight='bold') # Use Phi instead of Thorn (alas)
+    plt.text(
+        5,
+        0.87,
+        r"$(1-\wp^{1/\CRRA}\Phi_{R})\equiv \overline{\MPC}$",
+        fontsize=26,
+        fontweight="bold",
+    )  # Use Phi instead of Thorn (alas)
 
-plt.text(0.5,0.07,kappaDef,fontsize = 26,fontweight='bold')
-plt.text(8.05,0,"$m$",fontsize = 26)
-plt.arrow(1.45,0.61,-0.4,0,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(2.2,0.07,0.2,-0.01,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-plt.arrow(4.95,0.895,-0.2,0.03,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
+plt.text(0.5, 0.07, kappaDef, fontsize=26, fontweight="bold")
+plt.text(8.05, 0, "$m$", fontsize=26)
+plt.arrow(
+    1.45,
+    0.61,
+    -0.4,
+    0,
+    head_width=0.02,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    2.2,
+    0.07,
+    0.2,
+    -0.01,
+    head_width=0.02,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
+plt.arrow(
+    4.95,
+    0.895,
+    -0.2,
+    0.03,
+    head_width=0.02,
+    width=0.001,
+    facecolor="black",
+    length_includes_head="True",
+)
 
-make('MPCLimits')
+make("MPCLimits")
 
 
 # %% [markdown]
